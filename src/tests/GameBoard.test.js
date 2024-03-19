17f7fe7cf7b4921a25e202d5d4ceeec2fb1d7148
Oleg Kasenbekov <Oleg.Kasenbekov@enterra-inc.com>
2024-03-19 22:24:25 +0700

    game board add block test

--- src/tests/GameBoard.test.js
+++ src/tests/GameBoard.test.js
@@ -1,5 +1,6 @@
 import GameBoard from "../GameBoard.js";
 import {expect, describe, test, beforeAll} from "vitest";
+import Block from "../Block.js";
 
 let board;
 
@@ -11,8 +12,15 @@ describe("GameBoard", () => {
 	test("initialize board", () => {
 		expect(board.element.classList.contains('board')).toBe(true)
 		expect(board.cells).length(16)
-
+		expect(board.blocks).length(1)
+	})
+	test("get empty cell", () => {
 		const cell = board.getRandomEmptyCell()
 		expect(cell.isEmpty()).toBe(true)
+	})
+	test("add block", () => {
+		board.addBlock()
+		expect(board.blocks.length > 0).toBe(true)
+		expect(board.cells.filter((cell) => cell.isEmpty())).length(15)
 	})
 })
